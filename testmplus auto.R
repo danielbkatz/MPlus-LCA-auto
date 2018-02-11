f621883c98926606ddae2f31afac735693cfed65
danielbkatz <katzdanb@gmail.com>
2018-02-11 14:10:47 -0800

    Add files via upload

--- testmplus auto.R
+++ testmplus auto.R
@@ -1,6 +1,10 @@
-mplusbasicmix <- function(filename, ext, title_mp, filename_path, data_set, variableuse, categoricallist, missflag, classes, starts, refinestarts)
-{mplusinptoclass <- list()
+mplusbasicmix <- function(filename, ext, title_mp, namedata, data_set, variableuse, categoricallist=FALSE, missflag, classes, starts, refinestarts)
+{
+mplusinptoclass <- list()
 cl<- 1:classes
+
+tablefile <- paste(namedata, ".dat", sep="")
+write.table(data_set, file=tablefile, row.names=FALSE, col.names = FALSE, sep="\t", quote=FALSE)
 
 #should use a loop eventually instead. Get names of variables in data set
 varlistnames <- names(data_set)
@@ -17,9 +21,29 @@ cat.list.paste <- paste(cat.list, collapse="\n")
 #varlist <- deparse(substitute(varlist))
 filename1<- paste(filename, cl, ext, sep="")
 
+if(categoricallist==FALSE){
 #this creates the Mplus syntax
 fintitle <- paste("Title:", title_mp, ";", sep=" ")
-data <- paste("data: File is", filename_path, ";", sep=" ") 
+data <- paste("data: File is", tablefile, ";", sep=" ") 
+variablelist <- paste("Variable: Names are", (varlistpaste), ";")
+usev <- paste("Usev=", variableusepaste, ";")
+missflag <- paste("Missing are all","(",missflag,")", ";")
+analysis <- paste("Analysis: type=Mixture;")
+starts <- paste("starts=", starts, refinestarts, ";", sep =" ")
+processors <- "processors=4(starts);"
+output <- paste("Output:", "sampstat", "Tech11", "Tech14", ";") 
+plot <- paste("plot: type=plot3;", "series=", variableusepaste,"(*)", ";")
+for(i in 1:classes){
+  classes[[i]] <- paste("class=c","(", i, ")",";", sep="")
+  mplusinptoclass[[i]] <- paste(fintitle, data, variablelist, usev, missflag, classes[[i]], analysis, starts, processors, output, plot, sep="\n")}
+
+#create each mplus file
+for(q in 1:length(filename1)){
+  cat(mplusinptoclass[[q]], file = filename1[[q]])}}
+
+else{
+fintitle <- paste("Title:", title_mp, ";", sep=" ")
+data <- paste("data: File is", tablefile, ";", sep=" ") 
 variablelist <- paste("Variable: Names are", (varlistpaste), ";")
 usev <- paste("Usev=", variableusepaste, ";")
 categorical <- paste("categorical=", cat.list.paste,";")
@@ -29,16 +53,16 @@ starts <- paste("starts=", starts, refinestarts, ";", sep =" ")
 processors <- "processors=4(starts);"
 output <- paste("Output:", "sampstat", "Tech11", "Tech14", ";") 
 plot <- paste("plot: type=plot3;", "series=", variableusepaste,"(*)", ";")
-
-
-#this loops through each class enumeration
 for(i in 1:classes){
   classes[[i]] <- paste("class=c","(", i, ")",";", sep="")
   mplusinptoclass[[i]] <- paste(fintitle, data, variablelist, usev, categorical, missflag, classes[[i]], analysis, starts, processors, output, plot, sep="\n")}
 
 #create each mplus file
 for(q in 1:length(filename1)){
-  cat(mplusinptoclass[[q]], file = filename1[[q]])}
+  cat(mplusinptoclass[[q]], file = filename1[[q]])}}
+
+#this loops through each class enumeration
+
 
 #creating batch file
   bat.string <- paste("call mplus.exe ", filename, cl, ext, sep = "")
@@ -52,7 +76,10 @@ shell.exec(file.path(getwd(), bat.file.name))
 
 
 
-#example usage 
-mplusbasicmix("testrun", ".inp", "this is a test of my function", "c:/users/katz/desktop/r-source-scripts", formdclean, formduse, formduse, 999, 4, 500, 100 )
+#example usage for LPA 
+mplusbasicmix("testrun", ".inp", "this is a test of my function", "formdata", formdclean, formduse, categoricallist = FALSE, 999, 4, 500, 100 )
 
+#example usage for LCA
+
+mplusbasicmix("testrun", ".inp", "this is a test of my function",  "formdata", formdclean, formduse, formduse, 999, 4, 500, 100 )
 
