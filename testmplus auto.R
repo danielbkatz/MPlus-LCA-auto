f673b8e984db2fe21317957db2f7f2198bd878f2
danielbkatz <katzdanb@gmail.com>
2018-02-12 09:55:48 -0800

    Add files via upload

--- testmplus auto.R
+++ testmplus auto.R
@@ -1,4 +1,4 @@
-mplusbasicmix <- function(filename, ext, title_mp, namedata, data_set, variableuse, categoricallist=FALSE, missflag, classes, starts, refinestarts)
+mplusbasicmix <- function(filename, ext, title_mp, namedata, data_set, variableuse, missflag, classes, starts, refinestarts, categoricallist=NULL)
 {
   mplusinptoclass <- list()
   cl<- 1:classes
@@ -12,7 +12,9 @@ mplusbasicmix <- function(filename, ext, title_mp, namedata, data_set, variableu
     
   variableusenames <- names(variableuse)
   variableusepaste <- paste(variableusenames, collapse="\n")
-
+  
+  cat.null=is.null(categoricallist)
+  
   cat.list <- names(categoricallist)
   cat.list.paste <- paste(cat.list, collapse="\n")
 
@@ -20,29 +22,31 @@ mplusbasicmix <- function(filename, ext, title_mp, namedata, data_set, variableu
 #varlist <- paste(variablelist)
 #varlist <- deparse(substitute(varlist))
   filename1<- paste(filename, cl, ext, sep="")
+  
+  
+  #this is for creating the syntax for an LPA
+  if(cat.null==TRUE){
+    #this creates the Mplus syntax
+    fintitle <- paste("Title:", title_mp, ";", sep=" ")
+    data <- paste("data: File is", tablefile, ";", sep=" ") 
+    variablelist <- paste("Variable: Names are", (varlistpaste), ";")
+    usev <- paste("Usev=", variableusepaste, ";")
+    missflag <- paste("Missing are all","(",missflag,")", ";")
+    analysis <- paste("Analysis: type=Mixture;")
+    starts <- paste("starts=", starts, refinestarts, ";", sep =" ")
+    processors <- "processors=4(starts);"
+    output <- paste("Output:", "sampstat", "Tech11", "Tech14", ";") 
+    plot <- paste("plot: type=plot3;", "series=", variableusepaste,"(*)", ";")
+      for(i in 1:classes){
+        classes[[i]] <- paste("class=c","(", i, ")",";", sep="")
+        mplusinptoclass[[i]] <- paste(fintitle, data, variablelist, usev, missflag, classes[[i]], analysis, starts, processors, output, plot, sep="\n")}
+    
+    #create each mplus file
+    for(q in 1:length(filename1)){
+      cat(mplusinptoclass[[q]], file = filename1[[q]])}}
+  
 
-#this is for creating the syntax for an LPA
-if(categoricallist==FALSE){
-#this creates the Mplus syntax
-  fintitle <- paste("Title:", title_mp, ";", sep=" ")
-  data <- paste("data: File is", tablefile, ";", sep=" ") 
-  variablelist <- paste("Variable: Names are", (varlistpaste), ";")
-  usev <- paste("Usev=", variableusepaste, ";")
-  missflag <- paste("Missing are all","(",missflag,")", ";")
-  analysis <- paste("Analysis: type=Mixture;")
-  starts <- paste("starts=", starts, refinestarts, ";", sep =" ")
-  processors <- "processors=4(starts);"
-  output <- paste("Output:", "sampstat", "Tech11", "Tech14", ";") 
-  plot <- paste("plot: type=plot3;", "series=", variableusepaste,"(*)", ";")
-  for(i in 1:classes){
-    classes[[i]] <- paste("class=c","(", i, ")",";", sep="")
-    mplusinptoclass[[i]] <- paste(fintitle, data, variablelist, usev, missflag, classes[[i]], analysis, starts, processors, output, plot, sep="\n")}
-
-#create each mplus file
-for(q in 1:length(filename1)){
-  cat(mplusinptoclass[[q]], file = filename1[[q]])}}
-
-#this is for creating the syntax for an LCA. There's probably a smoother way of doing this. 
+#this is for creating the syntax for an LCA. There's probably a smoother way of doing this.
 else{
   fintitle <- paste("Title:", title_mp, ";", sep=" ")
   data <- paste("data: File is", tablefile, ";", sep=" ") 
@@ -61,10 +65,9 @@ else{
     mplusinptoclass[[i]] <- paste(fintitle, data, variablelist, usev, categorical, missflag, classes[[i]], analysis, starts, processors, output, plot, sep="\n")}
 
 #create each mplus file
-  for(q in 1:length(filename1)){
-    cat(mplusinptoclass[[q]], file = filename1[[q]])}}
+for(q in 1:length(filename1)){
+  cat(mplusinptoclass[[q]], file = filename1[[q]])}}
 
-#this loops through each class enumeration
 
 
 #creating batch file
@@ -73,17 +76,16 @@ else{
   cat(bat.string, file=bat.file.name, sep="\n")
 
 #running batch file
-shell.exec(file.path(getwd(), bat.file.name))
-}
+shell.exec(file.path(getwd(), bat.file.name))}
 
 
 
 
 
 #example usage for LPA # the extension code (".inp") is just for my own testing so I can test with text files, which is easier to deal with.
-mplusbasicmix("testrun", ".inp", "this is a test of my function", "formdata", formdclean, formduse, categoricallist = FALSE, 999, 4, 500, 100 )
+mplusbasicmix("testt", ".inp", "this is an mplus file", "formd", formdcleanuse, formdcleanuse, 999, 3, 100, 20)
 
 #example usage for LCA
 
-mplusbasicmix("testrun", ".inp", "this is a test of my function",  "formdata", formdclean, formduse, formduse, 999, 4, 500, 100 )
+mplusbasicmix("testrun", ".inp", "this is a test of my function",  "formdata", formdcleanuse, formdcleanuse, 999, 4, 500, 100, formdcleanuse )
 
